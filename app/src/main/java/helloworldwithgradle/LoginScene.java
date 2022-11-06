4dc80675394e4ad7679c62d08bf2151421be0201
Luiz-Godoy <luiz.godoy255@gmail.com>
2022-11-05 23:49:29 -0300

    Subindo

---
+++ app/src/main/java/helloworldwithgradle/LoginScene.java
@@ -0,0 +1,20 @@
+package helloworldwithgradle;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class LoginScene extends Scene {
+	
+	Label lbUserName = new Label("Nome do Usuario:");
+	TextField txtUserName = new TextField("Digite o username aqui...");
+
+	public LoginScene(VBox vbox, double width, double height) {
+		super(vbox, width, height);
+		
+		vbox.getChildren().add(lbUserName);
+		vbox.getChildren().add(txtUserName);
+	}
+
+}
