31b874e457c27d5667b08a5e2386f7860d8cbf65
Luiz-Godoy <luiz.godoy255@gmail.com>
2022-11-06 16:19:56 -0300

    Att

--- app/src/main/java/helloworldwithgradle/LoginScene.java
+++ app/src/main/java/helloworldwithgradle/LoginScene.java
@@ -10,11 +10,17 @@ public class LoginScene extends Scene {
 	Label lbUserName = new Label("Nome do Usuario:");
 	TextField txtUserName = new TextField("Digite o username aqui...");
 
+	Label lbUserPass = new Label("Senha:");
+	TextField txtUserPass = new TextField("Digite a senha aqui...");
+
 	public LoginScene(VBox vbox, double width, double height) {
 		super(vbox, width, height);
 		
 		vbox.getChildren().add(lbUserName);
 		vbox.getChildren().add(txtUserName);
+
+		vbox.getChildren().add(lbUserPass);
+		vbox.getChildren().add(txtUserPass);
 	}
 
 }
